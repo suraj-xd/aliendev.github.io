4466b3ec34b3c7b34034b6cd356986706c80d7cd
suraj-xd <75672977+suraj-xd@users.noreply.github.com>
2020-12-25 01:01:50 -0800

    Add files via upload

---
+++ jadoo.js
@@ -0,0 +1,72 @@
+function Sound1() {
+    let audio1 = document.querySelector('.sound1')
+    audio1.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound2() {
+    let audio2 = document.querySelector('.sound2')
+    audio2.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound3() {
+    let audio3 = document.querySelector('.sound3')
+    audio3.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound4() {
+    let audio4 = document.querySelector('.sound4')
+    audio4.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound5() {
+    let audio5 = document.querySelector('.sound5')
+    audio5.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound6() {
+    let audio6 = document.querySelector('.sound6')
+    audio6.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound7() {
+    let audio7 = document.querySelector('.sound7')
+    audio7.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
+function Sound8() {
+    let audio8 = document.querySelector('.sound8')
+    audio8.play()
+    let image = document.querySelector('img')
+    image.src = "PicsArt_12-25-10.59.20.png"
+    setTimeout(() => {
+        image.src = 'PicsArt_12-25-10.58.55.png'
+    }, 300);
+}
